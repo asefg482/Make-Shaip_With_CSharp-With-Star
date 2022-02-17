6b311f06ceef367b028d638b5d659679b178c073
Asef <71902115+asefg482@users.noreply.github.com>
2022-02-18 00:40:41 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Art_Show
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            for (int i = 0; i <= x; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = x; i >= 0; i--)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
